b589b46133da0c47692a7693a9da9282971886bc
Jirphat Saetae <137375099+JirphatAok@users.noreply.github.com>
2023-09-28 15:52:52 +0700

    lab

---
+++ Lab/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+for (int i = 0; i < 3; i++) {
+    //     printf ( "Student %d:\nName: %s\nID: %d\nScores: %f %f %f %f %f\nGrades: B+  C C+ D  D+\nAverage Scores: 63.4", i ,S[ i ].Name , &S[ i ].ID , &S[ i ].ScoreSub1 , &S[ i ].ScoreSub2 ,&S[ i ].ScoreSub3 ,&S[ i ].ScoreSub4 ) ;
+    // }
